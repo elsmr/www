8ec217bc2e187cdf4e8041edafe2b5893718dd4a
Elias Meire <elias@meire.dev>
2020-05-26 10:06:24 +0200

    Add image for social

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,9 @@ module.exports = {
     description:
       "Frontend developer based in Ghent, Belgium. I love JavaScript, React and Open Source. Currently I'm obsessing over: animation, a11y, theme-ui and Canvas/WebGL.",
     url: 'https://elsmr.dev',
-    imageUrl: 'elias.jpg',
-    imageHeight: 720,
-    imageWidth: 720,
+    imageUrl: 'social.jpg',
+    imageHeight: 1200,
+    imageWidth: 1200,
     email: 'elias@meire.dev',
     twitter: '@elsmr_',
     github: 'elsmr',
