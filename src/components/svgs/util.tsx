d8f36e936a211a4a90bbad68022b2ce82b3f5422
Elias Meire <elias@meire.dev>
2020-06-14 12:12:36 +0200

    Fix type case

--- src/components/svgs/util.tsx
+++ src/components/svgs/util.tsx
@@ -2,10 +2,8 @@ import styled, { StyledComponent } from '@emotion/styled'
 import React from 'react'
 import { Box, BoxOwnProps } from 'theme-ui'
 
-export const SVGBox = styled(Box)({ as: 'svg' }) as StyledComponent<
-  React.ComponentProps<'svg'>,
-  BoxOwnProps,
-  {}
->
+export const SVGBox = (styled(Box)({
+  as: 'svg',
+}) as unknown) as StyledComponent<React.ComponentProps<'svg'>, BoxOwnProps, {}>
 
 export type SVGBoxProps = React.ComponentProps<typeof SVGBox>
